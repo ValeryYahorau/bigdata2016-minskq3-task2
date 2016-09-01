86d9904ac60232f71fef1dc2b1a1b13233d376ef
ValeryYahorau <yehorav.valery@gmail.com>
2016-09-01 12:29:59 +0300

    Small changes

--- src/main/java/com/epam/bigdata2016/minskq3/task2/Client.java
+++ src/main/java/com/epam/bigdata2016/minskq3/task2/Client.java
@@ -31,94 +31,97 @@ import org.apache.hadoop.yarn.util.Records;
 
 public class Client {
 
-  Configuration conf = new YarnConfiguration();
-  
-  public void run(String[] args) throws Exception {
+    Configuration conf = new YarnConfiguration();
 
-    final int n = Integer.valueOf(args[0]);
-    final Path jarPath = new Path(args[1]);
-    System.out.println("STEP 1 " + n + " | " + args[1]);
-    // Create yarnClient
-    YarnConfiguration conf = new YarnConfiguration();
-    YarnClient yarnClient = YarnClient.createYarnClient();
-    yarnClient.init(conf);
-    yarnClient.start();
-    System.out.println("STEP 2 ");
-    // Create application via yarnClient
-    YarnClientApplication app = yarnClient.createApplication();
+    public void run(String[] args) throws Exception {
 
-    // Set up the container launch context for the application master
-    ContainerLaunchContext amContainer = Records.newRecord(ContainerLaunchContext.class);
-    amContainer.setCommands(
-        Collections.singletonList(
-            "$JAVA_HOME/bin/java" +
-            " -Xmx256M" +
-            " com.epam.bigdata2016.minskq3.task2.ApplicationMaster" +
-            " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" + 
-            " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr" 
-            )
+        //n - is number of containers,
+        final int n = Integer.valueOf(args[0]);
+        final Path jarPath = new Path(args[1]);
+
+        System.out.println("STEP 1 " + n + " | " + args[1]);
+        // Create yarnClient
+        YarnConfiguration conf = new YarnConfiguration();
+        YarnClient yarnClient = YarnClient.createYarnClient();
+        yarnClient.init(conf);
+        yarnClient.start();
+        System.out.println("STEP 2 ");
+        // Create application via yarnClient
+        YarnClientApplication app = yarnClient.createApplication();
+
+        // Set up the container launch context for the application master
+        ContainerLaunchContext amContainer = Records.newRecord(ContainerLaunchContext.class);
+        amContainer.setCommands(
+                Collections.singletonList(
+                        "$JAVA_HOME/bin/java" +
+                                " -Xmx256M" +
+                                " com.epam.bigdata2016.minskq3.task2.ApplicationMaster" +
+                                " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" +
+                                " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"
+                )
         );
-    
-    // Setup jar for ApplicationMaster
-    LocalResource appMasterJar = Records.newRecord(LocalResource.class);
-    setupAppMasterJar(jarPath, appMasterJar);
-    amContainer.setLocalResources(Collections.singletonMap("bigdata2016-minskq3-task2-1.2.0-jar-with-dependencies.jar", appMasterJar));
 
-    // Setup CLASSPATH for ApplicationMaster
-    Map<String, String> appMasterEnv = new HashMap<String, String>();
-    setupAppMasterEnv(appMasterEnv);
-    amContainer.setEnvironment(appMasterEnv);
-    
-    // Set up resource type requirements for ApplicationMaster
-    Resource capability = Records.newRecord(Resource.class);
-    capability.setMemory(256);
-    capability.setVirtualCores(1);
-    System.out.println("STEP 3");
-    // Finally, set-up ApplicationSubmissionContext for the application
-    ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
-    appContext.setApplicationName("bigdata2016-minskq3-task2"); // application name
-    appContext.setAMContainerSpec(amContainer);
-    appContext.setResource(capability);
-    appContext.setQueue("default"); // queue 
+        // Setup jar for ApplicationMaster
+        LocalResource appMasterJar = Records.newRecord(LocalResource.class);
+        setupAppMasterJar(jarPath, appMasterJar);
+        amContainer.setLocalResources(Collections.singletonMap("bigdata2016-minskq3-task2-1.2.0-jar-with-dependencies.jar", appMasterJar));
 
-    // Submit application
-    ApplicationId appId = appContext.getApplicationId();
-    System.out.println("Submitting application " + appId);
-    yarnClient.submitApplication(appContext);
-    System.out.println("STEP 4");
-    ApplicationReport appReport = yarnClient.getApplicationReport(appId);
-    YarnApplicationState appState = appReport.getYarnApplicationState();
-    while (appState != YarnApplicationState.FINISHED && 
-           appState != YarnApplicationState.KILLED && 
-           appState != YarnApplicationState.FAILED) {
-      Thread.sleep(100);
-      appReport = yarnClient.getApplicationReport(appId);
-      appState = appReport.getYarnApplicationState();
+        // Setup CLASSPATH for ApplicationMaster
+        Map<String, String> appMasterEnv = new HashMap<String, String>();
+        setupAppMasterEnv(appMasterEnv);
+        amContainer.setEnvironment(appMasterEnv);
+
+        // Set up resource type requirements for ApplicationMaster
+        Resource capability = Records.newRecord(Resource.class);
+        capability.setMemory(256);
+        capability.setVirtualCores(1);
+        System.out.println("STEP 3");
+        // Finally, set-up ApplicationSubmissionContext for the application
+        ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
+        appContext.setApplicationName("bigdata2016-minskq3-task2"); // application name
+        appContext.setAMContainerSpec(amContainer);
+        appContext.setResource(capability);
+        appContext.setQueue("default"); // queue
+
+        // Submit application
+        ApplicationId appId = appContext.getApplicationId();
+        System.out.println("Submitting application " + appId);
+        yarnClient.submitApplication(appContext);
+        System.out.println("STEP 4");
+        ApplicationReport appReport = yarnClient.getApplicationReport(appId);
+        YarnApplicationState appState = appReport.getYarnApplicationState();
+        while (appState != YarnApplicationState.FINISHED &&
+                appState != YarnApplicationState.KILLED &&
+                appState != YarnApplicationState.FAILED) {
+            Thread.sleep(100);
+            appReport = yarnClient.getApplicationReport(appId);
+            appState = appReport.getYarnApplicationState();
+        }
+        System.out.println("Application Diagnostic" + appReport.getDiagnostics());
+        System.out.println("Application " + appId + " finished with" + " state " + appState + " at " + appReport.getFinishTime());
     }
-    System.out.println("Application Diagnostic" + appReport.getDiagnostics());
-    System.out.println("Application " + appId + " finished with" + " state " + appState + " at " + appReport.getFinishTime());
-  }
-  
-  private void setupAppMasterJar(Path jarPath, LocalResource appMasterJar) throws IOException {
-    System.out.println("STEP X");
-    FileStatus jarStat = FileSystem.get(conf).getFileStatus(jarPath);
-    appMasterJar.setResource(ConverterUtils.getYarnUrlFromPath(jarPath));
-    appMasterJar.setSize(jarStat.getLen());
-    appMasterJar.setTimestamp(jarStat.getModificationTime());
-    appMasterJar.setType(LocalResourceType.FILE);
-    appMasterJar.setVisibility(LocalResourceVisibility.PUBLIC);
-  }
-  
-  private void setupAppMasterEnv(Map<String, String> appMasterEnv) {
-    System.out.println("STEP Y");
-    for (String c : conf.getStrings(YarnConfiguration.YARN_APPLICATION_CLASSPATH, YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH)) {
-      Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(), c.trim());
+
+    private void setupAppMasterJar(Path jarPath, LocalResource appMasterJar) throws IOException {
+        System.out.println("STEP X");
+        FileStatus jarStat = FileSystem.get(conf).getFileStatus(jarPath);
+        appMasterJar.setResource(ConverterUtils.getYarnUrlFromPath(jarPath));
+        appMasterJar.setSize(jarStat.getLen());
+        appMasterJar.setTimestamp(jarStat.getModificationTime());
+        appMasterJar.setType(LocalResourceType.FILE);
+        appMasterJar.setVisibility(LocalResourceVisibility.PUBLIC);
     }
-    Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(), Environment.PWD.$() + File.separator + "*");
-  }
-  
-  public static void main(String[] args) throws Exception {
-    Client c = new Client();
-    c.run(args);
-  }
+
+    private void setupAppMasterEnv(Map<String, String> appMasterEnv) {
+        System.out.println("STEP Y");
+        for (String c : conf.getStrings(YarnConfiguration.YARN_APPLICATION_CLASSPATH, YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH)) {
+            Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(), c.trim());
+        }
+        Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(), Environment.PWD.$() + File.separator + "*");
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("START CLIENT CREATION AND CONFIGURATION");
+        Client c = new Client();
+        c.run(args);
+    }
 }
