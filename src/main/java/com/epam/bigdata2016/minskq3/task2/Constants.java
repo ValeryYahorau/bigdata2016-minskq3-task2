1847b49060779e13e4243cd72438dbb16c794557
ValeryYahorau <yehorav.valery@gmail.com>
2016-09-08 18:00:32 +0300

    Small fixes

--- src/main/java/com/epam/bigdata2016/minskq3/task2/Constants.java
+++ src/main/java/com/epam/bigdata2016/minskq3/task2/Constants.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.fs.Path;
 
 public class Constants {
     public static final String HDFS_ROOT_PATH = "hdfs://sandbox.hortonworks.com:8020";
-    public static final Path HDFS_MY_APP_JAR_PATH = new Path(Constants.HDFS_ROOT_PATH + "/apps/bigdata2016-minskq3-task2-1.0.0-jar-with-dependencies.jar");
-    public static final String JAR_NAME = "bigdata2016-minskq3-task2-1.0.1-jar-with-dependencies.jar";
+    public static final Path HDFS_MY_APP_JAR_PATH = new Path(Constants.HDFS_ROOT_PATH + "/apps/" + Constants.JAR_NAME);
+    public static final String JAR_NAME = "bigdata2016-minskq3-task2-1.0.2-jar-with-dependencies.jar";
 }
 
